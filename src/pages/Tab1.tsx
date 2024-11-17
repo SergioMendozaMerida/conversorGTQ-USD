1bec70ffeda7f7b9565dff06527a5dc1e365656d
SergioMendozaMerida <darinelmendoza277@gmail.com>
2024-11-17 12:58:15 -0600

    1.2

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
 import { IonButton, IonContent, IonHeader, IonInput, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Tab1: React.FC = () => {
 
